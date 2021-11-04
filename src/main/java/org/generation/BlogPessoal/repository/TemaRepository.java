d9978919b2511d33408559d6465b516dffb4ba56
renatafloro <flororenataef@gmail.com>
2021-11-03 21:19:02 -0300

    CRUD 3- Blog pessoal

---
+++ src/main/java/org/generation/BlogPessoal/repository/TemaRepository.java
@@ -0,0 +1,10 @@
+package org.generation.BlogPessoal.repository;
+
+import java.util.List;
+
+import org.generation.BlogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+     public List<Tema> FindAllByDescricaoContainingIgnoreCase(String descricao);
+     }
