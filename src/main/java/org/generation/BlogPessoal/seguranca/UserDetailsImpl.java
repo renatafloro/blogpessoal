5b7c633f8b06962b6735670092f634862963fef7
Renata Floro <flororenataef@gmail.com>
2021-12-07 19:01:17 -0300

    Correção do problema de login do swagger no heroku

--- src/main/java/org/generation/BlogPessoal/seguranca/UserDetailsImpl.java
+++ src/main/java/org/generation/BlogPessoal/seguranca/UserDetailsImpl.java
@@ -1,6 +1,7 @@
 package org.generation.BlogPessoal.seguranca;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.generation.BlogPessoal.model.Usuario;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,6 +13,7 @@ public class UserDetailsImpl implements UserDetails {
 
 	private String userName;
 	private String password;
+	private List<GrantedAuthority> authorities;
 
 	public UserDetailsImpl(Usuario user) {
 		this.userName = user.getUsuario();
@@ -24,7 +26,7 @@ public class UserDetailsImpl implements UserDetails {
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
 
 	@Override
